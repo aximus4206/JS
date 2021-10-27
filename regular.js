884ab43813e71866bb4e3153811befe93ec11c78
aximus4206 <aximus4206@gmail.com>
2021-10-27 15:53:51 +0300

    posledniy raz

--- regular.js
+++ regular.js
@@ -1,13 +1,13 @@
 
-const checkPhone = function (tel){
-    return /^\+\d{2}\(\d{3}\)\d{3}(\-\d{2}){2}$/.test(tel);
+const checkPhone = function (phone){
+    return /^\+\d{2}\(\d{3}\)\d{3}(\-\d{2}){2}$/.test(phone);
 }
 
 const checkEmail = function (email){
     return /^([\w-]+[\d+]?)+@([\w-]+)\.([\w]+)\.(\w{3})$/.test(email);
 }
 
-const checkWebsite = function (site){
+const checkSite = function (site){
     return /^(https?)\:\/\/([\w-]+)\.([\w]{3})$/.test(site);
 }
 
