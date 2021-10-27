144a4e0b32177f2ab441d53b30d1769335c0fd23
aximus4206 <aximus4206@gmail.com>
2021-10-27 15:57:35 +0300

    VOT!

--- regular.js
+++ regular.js
@@ -1,20 +1,20 @@
 
 const checkPhone = function (phone){
     return /^\+\d{2}\(\d{3}\)\d{3}(\-\d{2}){2}$/.test(phone);
-}
+};
 
 const checkEmail = function (email){
     return /^([\w-]+[\d+]?)+@([\w-]+)\.([\w]+)\.(\w{3})$/.test(email);
-}
+};
 
 const checkSite = function (site){
     return /^(https?)\:\/\/([\w-]+)\.([\w]{3})$/.test(site);
-}
+};
 
 const checkPassword = function (password){
     return /^[a-zA-Z0-9_]{6,25}$/.test(password);
-}
+};
 
 const checkIpv4 = function (ipv4){
     return /^(\d{1,3}\.?){4}$/.test(ipv4);
-}
+};
