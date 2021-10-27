a4251a670914c281219e6840066c02f6ba04c4b0
aximus4206 <aximus4206@gmail.com>
2021-10-27 15:33:44 +0300

    regular exp

--- regular.js
+++ regular.js
@@ -2,9 +2,8 @@
 let tel = "+79(380)990-90-93";
 
 const phoneCheck = function (tel){
-    let phoneRegExp = /^\+\d{2}\(\d{3}\)\d{3}\-\d{2}\-\d{2}$/;
-    let result = phoneRegExp.test(tel);
-    return result;
+    let phoneRegExp = /^\+\d{2}\(\d{3}\)\d{3}(\-\d{2}){2}$/;
+    return phoneRegExp.test(tel);
 }
 phoneCheck(tel);
 
@@ -12,17 +11,15 @@ let email = "someone@something.some.com"
 
 const emailCheck = function (email){
     let emailRegExp = /^([\w-]+[\d+]?)+@([\w-]+)\.([\w]+)\.(\w{3})$/;
-    let result = emailRegExp.test(email);
-    return result;
+    return emailRegExp.test(email);
 }
 emailCheck(email);
 
 let site = "http://yaplakal.dno"
 
 const websiteCheck = function (site){
-    let siteRegExp = /^(http|https)\:\/\/([\w-]+)\.([\w]{3})$/;
-    let result = siteRegExp.test(site);
-    return result;
+    let siteRegExp = /^(https?)\:\/\/([\w-]+)\.([\w]{3})$/;
+    return siteRegExp.test(site);
 }
 websiteCheck(site);
 
@@ -30,16 +27,14 @@ let password = "Yaposhka5691";
 
 const passwordCheck = function (password){
     let passwordRegExp = /^[a-zA-Z0-9_]{6,25}$/;
-    let result = passwordRegExp.test(password);
-    return result;
+    return passwordRegExp.test(password);
 }
 passwordCheck(password);
 
 let ipv4 = "172.16.255.2"
 
 const ipv4Check = function (ipv4){
-    let ipv4RegExp = /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$/;
-    let result = ipv4RegExp.test(ipv4);
-    return result;
+    let ipv4RegExp = /^(\d{1,3}\.?){4}$/;
+    return ipv4RegExp.test(ipv4);
 }
 passwordCheck(ipv4);
